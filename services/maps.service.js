5ff12a84231c06b0ef0cbff6b4a4bd3024457cc1
Yadhav M <yadhavmundekkat@gmail.com>
2025-09-16 21:14:30 +0530

    Getting cords and getting suggestions updated

--- services/maps.service.js
+++ services/maps.service.js
@@ -1,36 +1,29 @@
 const axios = require('axios')
 const captainModel = require('../models/captain.model')
 
-module.exports.getCoordinates = async (address) =>{
-    
-    if(!address) { 
-        throw new Error('Address is required')
+module.exports.getCoordinates = async (address) => {
+  if (!address) {
+    throw new Error('Address is required');
+  }
+  try {
+    console.log(address);
+    const response = await axios.get(`https://photon.komoot.io/api/?q=${encodeURIComponent(address)}&limit=1`);
+    console.log(response.data.features);
+    if (response.status === 200 && response.data.features.length > 0) {
+      const location = response.data.features[0];
+      const [lon, lat] = location.geometry.coordinates;
+      return {
+        lat: lat,
+        lon: lon,
+      };
+    } else {
+      throw new Error('Unable to get Coordinates');
     }
-    try{
-        const response = await axios.get(`https://nominatim.openstreetmap.org/search?q=${address}&format=json&limit=1` , {
-            headers: {
-                'User-Agent': 'Uber-clone/1.0 (yadhavmundekkat@gmail.com)'
-            }
-        })
-
-        if(response.status == 200) { 
-            const location = response.data[0]
-            return { 
-                lat : location.lat , 
-                lon : location.lon,
-            }
-        }else { 
-            throw new Error('Unable to get Coordinates')
-        }
-        
-    }catch(err) { 
-
-        console.log(error) 
-
-        throw error
-
-    }
-}
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
 
 module.exports.getDistanceTime = async (origin,destination) => {
     if(!origin || !destination) { 
@@ -65,7 +58,7 @@ module.exports.getSuggestions = async (address)=>{
     }
 
     try { 
-        const response = await axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=${address}`, {
+        const response = await axios.get(`https://photon.komoot.io/api/?q=${encodeURIComponent(address)}&limit=10`, {
             headers: {
                 'User-Agent': 'Uber-clone/1.0 (yadhavmundekkat@gmail.com)'
             }
